309ce69dc6742190eebeed8e94f1d0198f0460a9
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-04-29 22:19:33 +0300

    promises

--- checkout_page.js
+++ checkout_page.js
@@ -4,7 +4,7 @@ const vipHouse = document.querySelector('.mphb-room-details input');
 let options = [];
 let optionsChild = [];
 
-//console.log(`value`, vipHouse)
+//console.log(`value`, vipHouse) promises
 let condition = true;
 let condition2 = true;
 
